422fa5a5c42a41e4f2f70c912c87e61d7d26e35e
Talal Arshad <talal7860@gmail.com>
2015-09-03 20:45:14 +0500

    login redirect loop issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -89,7 +89,7 @@ class ApplicationController < ActionController::Base
   
   def back_or_default_url(default)
     url = session[:return_to] || default
-    url = default if url == login_url
+    url = default if url == login_url || url.include?("login")
     session[:return_to] = nil
     url
   end    
