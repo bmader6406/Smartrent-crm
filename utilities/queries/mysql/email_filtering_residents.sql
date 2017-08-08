ed8f7606cdd3ca21ce652ca579c7ffe17c885563
mufeed <mufeed@qburst.com>
2017-08-08 15:54:53 +0530

    correction in mysql query

--- utilities/queries/mysql/email_filtering_residents.sql
+++ utilities/queries/mysql/email_filtering_residents.sql
@@ -1,4 +1,4 @@
 -- To update all resident emails to mithuns+[resident_email]@qburst.com
 
 UPDATE smartrent_residents SET email = CONCAT('temp+',email) WHERE email NOT LIKE '%+%';
-UPDATE smartrent_residents SET email = CONCAT('mithuns',SUBSTRING(CONCAT(LEFT(email, INSTR(email, '@')), 'qburst.com'),INSTR( email,  '+' )));
+UPDATE smartrent_residents SET email = CONCAT('mithuns',SUBSTRING(CONCAT(LEFT(email, INSTR(email, '@')), 'qburst.com'),INSTR( email,  '+' ))) WHERE email NOT LIKE '%mithuns%';
