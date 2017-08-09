cf605132dcc0781f1babe6c90aa1b806f470eaf7
mufeed <mufeed@qburst.com>
2017-08-09 12:12:38 +0530

    Removed entity names appearing in crm application

--- app/assets/javascripts/backbone/models/resident.js
+++ app/assets/javascripts/backbone/models/resident.js
@@ -6,8 +6,10 @@ Crm.Models.Resident = Backbone.Model.extend({
     }
   },
   
-  initialize: function() {
-
-	}
+  initialize: function() { 	
+     this.set('name',App.Utils.htmlDecode(this.get('name')));
+     this.set('full_name',App.Utils.htmlDecode(this.get('full_name')));
+     this.set('property_name',App.Utils.htmlDecode(this.get('property_name')));
+  }
 	
 });
