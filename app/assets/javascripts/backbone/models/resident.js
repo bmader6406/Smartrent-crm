a795ea73e5e01b718d2811893e56be40645d913a
Vishnu K <vishnuk@qburst.com>
2017-10-16 12:19:09 +0530

    Fixed colon issue in Residents listing screen

--- app/assets/javascripts/backbone/models/resident.js
+++ app/assets/javascripts/backbone/models/resident.js
@@ -7,9 +7,10 @@ Crm.Models.Resident = Backbone.Model.extend({
   },
   
   initialize: function() { 	
-     this.set('name',App.Utils.htmlDecode(this.get('name')));
-     this.set('full_name',App.Utils.htmlDecode(this.get('full_name')));
-     this.set('property_name',App.Utils.htmlDecode(this.get('property_name')));
-  }
-	
+   this.set('name',App.Utils.htmlDecode(this.get('name')));
+   this.set('email',App.Utils.htmlDecode(this.get('email')));
+   this.set('full_name',App.Utils.htmlDecode(this.get('full_name')));
+   this.set('property_name',App.Utils.htmlDecode(this.get('property_name')));
+ }
+ 
 });
